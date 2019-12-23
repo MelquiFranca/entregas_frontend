9d1e9ecccd63f63c3702e5c8c060f71f09833123
Melqui Franca <melquisedeque.pereira@contraprova.com.br>
2019-12-23 14:56:49 -0300

    URL API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://ec2-54-163-234-44.compute-1.amazonaws.com:5432',
+    baseURL: 'https://entregasback.herokuapp.com',
 });
 
 export default api;
